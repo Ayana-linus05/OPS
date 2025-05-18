d1b8826412bedb0118de3d6def9c2cab41c13836
Lanaya Sunil <lanayasunil097@gmail.com>
2025-05-19 02:16:17 +0530

    Update add2num.py

--- add2num.py
+++ add2num.py
@@ -1,9 +1,10 @@
 # Input: Get two numbers from the user
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the second number: "))
 
 # Add the numbers
-sum = num1 + num2
+sum = num1 + num2 + num3
 
 # Output: Display the result
 print("The sum of", num1, "and", num2, "is:", sum)
